fbba0e97b1c287eed32133408d7694c28076f27a
Nadine0109 <nadine0109@ya.ru>
2021-02-12 21:47:55 +0500

    BmiService.java, Main.java modified

--- BmiService.java
+++ BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate (double weight, double height) {
-        double bMI = weight / (height*height);
-        return bMI;
+    public double calculate(double weight, double height) {
+        double bmi = weight / (height * height);
+        return bmi;
     }
 }
--- Main.java
+++ Main.java
@@ -2,17 +2,17 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
-        double bMI1 = service.calculate (76, 1.75);
-        System.out.println(bMI1);
+        double bmi1 = service.calculate(76, 1.75);
+        System.out.println(bmi1);
 
-        double bMI2 = service.calculate (98, 1.98);
-        System.out.println(bMI2);
+        double bmi2 = service.calculate(98, 1.98);
+        System.out.println(bmi2);
 
-        double bMI3 = service.calculate (50, 1.82);
-        System.out.println(bMI3);
+        double bmi3 = service.calculate(50, 1.82);
+        System.out.println(bmi3);
 
-        double bMI4 = service.calculate (65, 1.42);
-        System.out.println(bMI4);
+        double bmi4 = service.calculate(65, 1.42);
+        System.out.println(bmi4);
     }
 
 }
